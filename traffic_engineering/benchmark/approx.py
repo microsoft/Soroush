c06e800a6b8df0b5283af62d044d43a46fca2ca2
PooriaNamyar <namyar@usc.edu>
2022-08-26 08:28:27 +0000

    approx waterfilling eval code

--- traffic_engineering/benchmark/approx.py
+++ traffic_engineering/benchmark/approx.py
@@ -11,12 +11,17 @@ from utilities import shortest_paths, constants
 from utilities import utils
 from ncflow.lib.problem import Problem
 
-
+# problem: Problem, num_paths_per_flow, num_iter_approx_water, link_cap, path_characteristics=None,
+#               path_edge_idx_mapping=None, path_path_len_mapping=None, break_down=False
 def get_rates(approach, problem, path_output, num_paths_per_flow, num_approx_iter, num_bet_iter, link_cap):
     path_dict, path_edge_idx_mapping, path_path_len_mapping, path_total_path_len_mapping, path_split_ratio_mapping = path_output
     if approach == constants.APPROX:
-        fid_to_flow_rate_mapping, dur = approx_waterfilling.get_rates(problem, path_edge_idx_mapping, num_paths_per_flow,
-                                                                      num_iter=num_approx_iter, link_cap=link_cap)
+        fid_to_flow_rate_mapping, dur, run_time_dict = approx_waterfilling.get_rates(problem, num_paths_per_flow,
+                                                                                     num_iter_approx_water=num_approx_iter,
+                                                                                     link_cap=link_cap,
+                                                                                     path_edge_idx_mapping=path_edge_idx_mapping,
+                                                                                     path_path_len_mapping=path_path_len_mapping)
+        
     elif approach == constants.APPROX_MCF:
         fid_to_flow_rate_mapping, dur = approx_water_plus_mcf.get_rates(problem, path_dict, path_edge_idx_mapping,
                                                                         num_paths_per_flow, num_iter=num_approx_iter,
@@ -45,26 +50,39 @@ def get_rates(approach, problem, path_output, num_paths_per_flow, num_approx_ite
     else:
         raise Exception(f"approach {approach} does not exist!")
 
-    return fid_to_flow_rate_mapping, dur
+    return fid_to_flow_rate_mapping, dur, run_time_dict
 
 
-# TM_MODEL_LIST = ['bimodal', 'gravity']
-TM_MODEL_LIST = ['bimodal', 'gravity']
-TOPO_NAME_LIST = ['Cogentco.graphml', 'GtsCe.graphml']
+TM_MODEL_LIST = [
+    'uniform',
+    'bimodal',
+    'gravity',
+    'poisson-high-inter',
+    # 'poisson-high-intra',
+]
+TOPO_NAME_LIST = [
+    'Uninett2010.graphml',
+    'Cogentco.graphml',
+    'GtsCe.graphml',
+    'UsCarrier.graphml',
+    'Colt.graphml',
+    'TataNld.graphml',
+    # 'Kdl.graphml',
+]
 # APPROACH_LIST = [constants.APPROX_MCF, constants.APPROX_BET, constants.APPROX_BET_MCF]
 # APPROACH_LIST = [constants.APPROX_BET, constants.APPROX_BET_MCF]
-APPROACH_LIST = [constants.APPROX_BET_MCF]
+APPROACH_LIST = [
+    constants.APPROX,
+    # constants.APPROX_BET_MCF,
+]
+# num_path_list = [4, 16]
 num_path_list = [16]
-# num_iter_list = [1, 5, 10]
 num_iter_approx_list = [1]
 num_iter_bet_list = [10]
 link_cap = 1000.0
 log_dir = "../outputs"
 base_split = 0.9
 split_type = constants.EXPONENTIAL_DECAY
-
-# current = 0
-# skip_until = 70
 
 
 for approach in APPROACH_LIST:
@@ -82,6 +100,7 @@ for approach in APPROACH_LIST:
                                                                                     k=num_paths, number_processors=32,
                                                                                     base_split=base_split,
                                                                                     split_type=split_type)
+
                 for file_name in fnames:
                     problem = Problem.from_file(file_name[3], file_name[4])
                     utils.revise_list_commodities(problem)
@@ -89,20 +108,22 @@ for approach in APPROACH_LIST:
                         for num_iter_bet in num_iter_bet_list:
                             log_file = log_file.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
                             log_folder_flows = log_folder_flows.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
-                            # if current < skip_until:
-                            #     current += 1
-                            #     continue
+
                             per_flow_log_file_name = file_name[4].split("/")[-1][:-4] + f"_num_paths_{num_paths}"
                             per_flow_log_file_name += f"_iter_approx_{num_iter_approx}_iter_bet_{num_iter_bet}"
                             print("=" * 5, file_name, per_flow_log_file_name, "=" * 5)
-                            fid_to_flow_rate_mapping, dur = get_rates(approach, problem, path_output, num_paths,
+                            approx_output = get_rates(approach, problem, path_output, num_paths,
                                                                       num_iter_approx, num_iter_bet, link_cap)
+                            fid_to_flow_rate_mapping, dur, run_time_dict = approx_output
                             total_flow = 0
                             for (src, dst) in fid_to_flow_rate_mapping:
                                 total_flow += np.sum(fid_to_flow_rate_mapping[src, dst])
 
-                            per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + ".pickle"
+                            per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_per_path_flow.pickle"
                             utils.write_to_file_as_pickle(fid_to_flow_rate_mapping, log_folder_flows, per_flow_log_file_path)
+
+                            run_time_dict_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_run_time_dict.pickle"
+                            utils.write_to_file_as_pickle(run_time_dict, log_folder_flows, run_time_dict_file_path)
 
                             log_txt = f"{file_name},{num_paths},{num_iter_approx},{num_iter_bet},{total_flow}," \
                                       f"{dur}, {per_flow_log_file_name}\n"
