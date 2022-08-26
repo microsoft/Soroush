da67045d1be1447136dc32133f556385ede921c4
PooriaNamyar <namyar@usc.edu>
2022-08-26 03:44:51 +0000

    adding danna evaluation code

--- traffic_engineering/alg/k_waterfilling.py
+++ traffic_engineering/alg/k_waterfilling.py
@@ -7,16 +7,34 @@ from utilities import constants
 from alg import waterfilling_utils
 from ncflow.lib.problem import Problem
 
+MODEL_TIME = "model"
+COMPUTATION_TIME = "computation"
+EXTRACT_RATE = "extract_rate"
 
-def get_max_min_fair_multi_path_constrained_k_waterfilling(problem: Problem, path_edge_idx_mapping, num_paths_per_flow,
-                                                           link_cap, k="inf", break_down=False):
+
+def get_max_min_fair_multi_path_constrained_k_waterfilling(problem: Problem, num_paths_per_flow, link_cap,
+                                                           k="inf", path_characteristics=None, path_edge_idx_mapping=None,
+                                                           path_path_len_mapping=None, break_down=False):
+    run_time_dict = dict()
+    run_time_dict[MODEL_TIME] = 0
+    run_time_dict[EXTRACT_RATE] = 0
+    run_time_dict[COMPUTATION_TIME] = 0
+
+    assert k == "inf" or k == int(k)
     st_time = datetime.now()
-    # assert k == "inf" or k == int(k)
-    output = waterfilling_utils.get_routing_matrix(problem, path_edge_idx_mapping, num_paths_per_flow, link_cap)
-    routing_matrix, capacity_vector, fid_to_sub_fid_mapping = output
+    if path_characteristics is None:
+        path_characteristics = waterfilling_utils.get_vectorized_path_characteristics(problem, path_edge_idx_mapping,
+                                                                                      path_path_len_mapping,
+                                                                                      num_paths_per_flow)
+    sub_fid_path_len_vector, edge_idx_vector, demand_vector, path_exist_vector, num_sub_flows = path_characteristics
+    output = waterfilling_utils.get_routing_matrix(problem, num_paths_per_flow, sub_fid_path_len_vector,
+                                                   edge_idx_vector, demand_vector, num_sub_flows, link_cap,
+                                                   return_details=False)
+    routing_matrix, capacity_vector = output
     if break_down:
         checkpoint_1_time = datetime.now()
     # print(f"creating traffic;", (datetime.now() - st_time).total_seconds())
+    list_commodities = problem.sparse_commodity_list
     num_flows = len(problem.sparse_commodity_list) * num_paths_per_flow
     per_flow_rate = np.zeros(shape=num_flows)
     flow_id_to_flow_rate_mapping = defaultdict(list)
@@ -25,6 +43,10 @@ def get_max_min_fair_multi_path_constrained_k_waterfilling(problem: Problem, pat
     np_flow_list = np.arange(num_flows)
 
     num_iterations = 0
+
+    run_time_dict[MODEL_TIME] = (datetime.now() - st_time).total_seconds()
+    computation_st_time = datetime.now()
+
     while num_flows_per_link.shape[0] and np.max(num_flows_per_link) > 0:
         num_iterations += 1
         current_fair_share = capacity_vector / num_flows_per_link
@@ -58,15 +80,21 @@ def get_max_min_fair_multi_path_constrained_k_waterfilling(problem: Problem, pat
             capacity_vector = np.compress(mask, capacity_vector)
             num_flows_per_link = routing_matrix @ np.ones(shape=per_flow_rate.shape[0])
 
+    run_time_dict[COMPUTATION_TIME] = (datetime.now() - computation_st_time).total_seconds()
+
     if break_down:
         checkpoint_2_time = datetime.now()
         dur = ((checkpoint_1_time - st_time).total_seconds(), (checkpoint_2_time - checkpoint_1_time).total_seconds())
     else:
         dur = (datetime.now() - st_time).total_seconds()
-    for fid, (src, dst, demand) in problem.sparse_commodity_list:
-        for sub_fid in fid_to_sub_fid_mapping[fid]:
-            flow_id_to_flow_rate_mapping[(src, dst)].append(final_flow_rate[sub_fid])
+
+    extract_st_time = datetime.now()
+    for fid, (src, dst, demand) in list_commodities:
+        sub_fid_list = np.arange(fid * num_paths_per_flow, (fid + 1) * num_paths_per_flow)
+        flow_id_to_flow_rate_mapping[(src, dst)] = final_flow_rate[sub_fid_list]
         assert sum(flow_id_to_flow_rate_mapping[src, dst]) <= demand + constants.O_epsilon
+    run_time_dict[EXTRACT_RATE] = (datetime.now() - extract_st_time).total_seconds()
     print(f"{k}-waterfilling num iterations;", num_iterations, dur)
-    return flow_id_to_flow_rate_mapping, dur
+    print(f"{k}-waterilling detailed run time {run_time_dict}")
+    return flow_id_to_flow_rate_mapping, dur, run_time_dict
 
--- traffic_engineering/benchmark/k_water.py
+++ traffic_engineering/benchmark/k_water.py
@@ -10,15 +10,33 @@ from utilities import utils
 from ncflow.lib.problem import Problem
 
 
-TM_MODEL_LIST = ['bimodal', 'gravity']
-TOPO_NAME_LIST = ['Uninett2010.graphml', 'Cogentco.graphml', 'GtsCe.graphml']
+# TM_MODEL_LIST = ['bimodal', 'gravity']
+# TOPO_NAME_LIST = ['Uninett2010.graphml', 'Cogentco.graphml', 'GtsCe.graphml']
+
+TM_MODEL_LIST = [
+    'uniform',
+    'bimodal',
+    'gravity',
+    'poisson-high-inter',
+    # 'poisson-high-intra',
+]
+TOPO_NAME_LIST = [
+    'Uninett2010.graphml',
+    'Cogentco.graphml',
+    'GtsCe.graphml',
+    'UsCarrier.graphml',
+    'Colt.graphml',
+    'TataNld.graphml',
+    # 'Kdl.graphml',
+]
+
 # num_path_list = [4, 16]
-num_path_list = [4]
+num_path_list = [16]
 link_cap = 1000.0
 log_dir = "../outputs"
 fid = utils.get_fid()
-log_file = f"../outputs/one_water_{fid}.txt"
-log_folder_flows = f"../outputs/one_water_{fid}/"
+log_file = f"../outputs/one_water_filling_{fid}.txt"
+log_folder_flows = f"../outputs/one_water_filling_{fid}/"
 k_k_water = 1
 
 for num_paths in num_path_list:
@@ -29,23 +47,29 @@ for num_paths in num_path_list:
             problem = Problem.from_file(example_file[3], example_file[4])
             path_output = shortest_paths.all_pair_shortest_path_multi_processor(problem.G, problem.edge_idx,
                                                                                 k=num_paths, number_processors=32)
-            path_dict, path_edge_idx_mapping, _, _, _ = path_output
+            path_dict, path_edge_idx_mapping, path_path_len_mapping, _, _ = path_output
             for file_name in fnames:
                 problem = Problem.from_file(file_name[3], file_name[4])
                 per_flow_log_file_name = file_name[4].split("/")[-1][:-4] + f"_num_paths_{num_paths}"
                 print("=" * 5, file_name, per_flow_log_file_name, "=" * 5)
-                k_water_fid_to_flow_rate_mapping, water_dur = k_waterfilling.get_max_min_fair_multi_path_constrained_k_waterfilling(problem,
-                                                                                                                                    path_edge_idx_mapping,
-                                                                                                                                    num_paths_per_flow=num_paths,
-                                                                                                                                    link_cap=link_cap,
-                                                                                                                                    k=k_k_water)
+                k_waterfilling_output = k_waterfilling.get_max_min_fair_multi_path_constrained_k_waterfilling(problem,
+                                                                                                              num_paths_per_flow=num_paths,
+                                                                                                              link_cap=link_cap,
+                                                                                                              k=k_k_water,
+                                                                                                              path_edge_idx_mapping=path_edge_idx_mapping,
+                                                                                                              path_path_len_mapping=path_path_len_mapping)
 
+                k_water_fid_to_flow_rate_mapping, k_water_dur, k_water_run_time_dict = k_waterfilling_output
                 k_water_total_flow = 0
                 for (src, dst) in k_water_fid_to_flow_rate_mapping:
                     k_water_total_flow += sum(k_water_fid_to_flow_rate_mapping[src, dst])
 
-                per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + ".pickle"
+                per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_per_path_flow.pickle"
                 utils.write_to_file_as_pickle(k_water_fid_to_flow_rate_mapping, log_folder_flows, per_flow_log_file_path)
-                log_txt = f"{file_name},{num_paths},{k_water_total_flow},{water_dur},{per_flow_log_file_name}\n"
+
+                run_time_dict_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_run_time_dict.pickle"
+                utils.write_to_file_as_pickle(k_water_run_time_dict, log_folder_flows, run_time_dict_file_path)
+
+                log_txt = f"{file_name},{num_paths},{k_water_total_flow},{k_water_dur},{per_flow_log_file_name}\n"
                 utils.write_to_file(log_txt, log_dir, log_file)
                 print("result:", log_txt)
