411220abf43e1bd6932840c2792b0eddcbd58f77
PooriaNamyar <namyar@usc.edu>
2022-09-06 19:00:09 +0000

    updating approx benchmark

--- traffic_engineering/benchmark/approx.py
+++ traffic_engineering/benchmark/approx.py
@@ -95,11 +95,11 @@ TM_MODEL_LIST = [
     # 'poisson-high-intra',
 ]
 TOPO_NAME_LIST = [
-    'Uninett2010.graphml',
-    'Cogentco.graphml',
-    'GtsCe.graphml',
+    # 'Uninett2010.graphml',
+    # 'Cogentco.graphml',
+    # 'GtsCe.graphml',
     'UsCarrier.graphml',
-    'Colt.graphml',
+    # 'Colt.graphml',
     'TataNld.graphml',
     # 'Kdl.graphml',
 ]
@@ -107,28 +107,25 @@ TOPO_NAME_LIST = [
 # APPROACH_LIST = [constants.APPROX_BET, constants.APPROX_BET_MCF]
 APPROACH_LIST = [
     # constants.APPROX,
-    # constants.APPROX_BET,
+    constants.APPROX_BET,
     constants.APPROX_BET_MCF,
 ]
 # num_path_list = [4, 16]
 mcf_grb_method = 2
 num_path_list = [16]
-num_iter_approx_list = [1]
-num_iter_bet_list = [10]
 link_cap = 1000.0
 log_dir = "../outputs"
-base_split = 0.9
 split_type = constants.EXPONENTIAL_DECAY
 num_repeats = 3
 
 # (num_bins, min_epsilon, min_beta, k, link_cap_scale_factor)
 TOPOLOGY_TO_APPROX_BET_MCF_PARAMS = {
-    'Uninett2010.graphml': (4, 1e-2, 1e-2, 1, 1000),
-    'Cogentco.graphml': (15, 1e-2, 1e-2, 1, 1000),
-    'GtsCe.graphml': (12, 1e-2, 1e-2, 1, 1000),
-    'UsCarrier.graphml': (7, 1e-2, 1e-2, 1, 1000),
-    'Colt.graphml': (10, 1e-2, 1e-2, 1, 1000),
-    'TataNld.graphml': (15, 1e-2, 1e-2, 1, 1000),
+    'Uninett2010.graphml': (4, 1e-2, 1e-2, 1, 1000, 1, 10, 0.9),
+    'Cogentco.graphml': (15, 1e-2, 1e-2, 1, 1000, 1, 10, 0.9),
+    'GtsCe.graphml': (12, 1e-2, 1e-2, 1, 1000, 1, 10, 0.9),
+    'UsCarrier.graphml': (7, 1e-2, 1e-2, 1, 1000, 1, 20, 1.0),
+    'Colt.graphml': (10, 1e-2, 1e-2, 1, 1000, 1, 10, 0.9),
+    'TataNld.graphml': (15, 1e-2, 1e-2, 1, 1000, 1, 20, 1.0),
     # 'Kdl.graphml',
 }
 
@@ -149,6 +146,7 @@ for approach in APPROACH_LIST:
                 fnames = utils.find_topo_tm_fname(topo_name, tm_model)
                 example_file = fnames[0]
                 problem = Problem.from_file(example_file[3], example_file[4])
+                num_bins, min_epsilon, min_beta, k, link_cap_scale_factor, num_iter_approx, num_iter_bet, base_split = TOPOLOGY_TO_APPROX_BET_MCF_PARAMS[topo_name]
                 path_output = shortest_paths.all_pair_shortest_path_multi_processor(problem.G, problem.edge_idx,
                                                                                     k=num_paths, number_processors=32,
                                                                                     base_split=base_split,
@@ -157,35 +155,33 @@ for approach in APPROACH_LIST:
                 for file_name in fnames:
                     problem = Problem.from_file(file_name[3], file_name[4])
                     utils.revise_list_commodities(problem)
-                    for num_iter_approx in num_iter_approx_list:
-                        for num_iter_bet in num_iter_bet_list:
-                            log_file = log_file.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
-                            log_folder_flows = log_folder_flows.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
 
-                            per_flow_log_file_name = file_name[4].split("/")[-1][:-4] + f"_num_paths_{num_paths}"
-                            per_flow_log_file_name += f"_iter_approx_{num_iter_approx}_iter_bet_{num_iter_bet}"
-                            print("=" * 5, file_name, per_flow_log_file_name, "=" * 5)
-                            num_bins, min_epsilon, min_beta, k, link_cap_scale_factor = TOPOLOGY_TO_APPROX_BET_MCF_PARAMS[topo_name]
-                            approx_output = get_rates(approach, problem, path_output, num_paths,
-                                                      num_iter_approx, num_iter_bet, link_cap,
-                                                      mcf_grb_method=mcf_grb_method,
-                                                      num_bins=num_bins,
-                                                      min_beta=min_beta,
-                                                      min_epsilon=min_epsilon,
-                                                      k=k,
-                                                      link_cap_scale_factor=link_cap_scale_factor)
-                            fid_to_flow_rate_mapping, dur, run_time_dict = approx_output
-                            total_flow = 0
-                            for (src, dst) in fid_to_flow_rate_mapping:
-                                total_flow += np.sum(fid_to_flow_rate_mapping[src, dst])
+                    log_file = log_file.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
+                    log_folder_flows = log_folder_flows.format(waterfilling_utils.get_approx_label(approach, (num_iter_approx, num_iter_bet)))
 
-                            per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_per_path_flow.pickle"
-                            utils.write_to_file_as_pickle(fid_to_flow_rate_mapping, log_folder_flows, per_flow_log_file_path)
+                    per_flow_log_file_name = file_name[4].split("/")[-1][:-4] + f"_num_paths_{num_paths}"
+                    per_flow_log_file_name += f"_iter_approx_{num_iter_approx}_iter_bet_{num_iter_bet}"
+                    print("=" * 5, file_name, per_flow_log_file_name, "=" * 5)
+                    approx_output = get_rates(approach, problem, path_output, num_paths,
+                                              num_iter_approx, num_iter_bet, link_cap,
+                                              mcf_grb_method=mcf_grb_method,
+                                              num_bins=num_bins,
+                                              min_beta=min_beta,
+                                              min_epsilon=min_epsilon,
+                                              k=k,
+                                              link_cap_scale_factor=link_cap_scale_factor)
+                    fid_to_flow_rate_mapping, dur, run_time_dict = approx_output
+                    total_flow = 0
+                    for (src, dst) in fid_to_flow_rate_mapping:
+                        total_flow += np.sum(fid_to_flow_rate_mapping[src, dst])
 
-                            run_time_dict_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_run_time_dict.pickle"
-                            utils.write_to_file_as_pickle(run_time_dict, log_folder_flows, run_time_dict_file_path)
+                    per_flow_log_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_per_path_flow.pickle"
+                    utils.write_to_file_as_pickle(fid_to_flow_rate_mapping, log_folder_flows, per_flow_log_file_path)
 
-                            log_txt = f"{file_name},{num_paths},{num_iter_approx},{num_iter_bet},{total_flow}," \
-                                      f"{dur}, {per_flow_log_file_name}\n"
-                            utils.write_to_file(log_txt, log_dir, log_file)
-                            print("results:", log_txt)
+                    run_time_dict_file_path = log_folder_flows + "/" + per_flow_log_file_name + "_run_time_dict.pickle"
+                    utils.write_to_file_as_pickle(run_time_dict, log_folder_flows, run_time_dict_file_path)
+
+                    log_txt = f"{file_name},{num_paths},{num_iter_approx},{num_iter_bet},{total_flow}," \
+                              f"{dur}, {per_flow_log_file_name}\n"
+                    utils.write_to_file(log_txt, log_dir, log_file)
+                    print("results:", log_txt)
