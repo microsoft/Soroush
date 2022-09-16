aa6e6a429f7f3734fdd9b8ef5218bdba3e19b24e
PooriaNamyar <namyar@usc.edu>
2022-09-16 07:09:45 +0000

    minor bug fix

--- traffic_engineering/benchmark/impact_bins_equi_binner.py
+++ traffic_engineering/benchmark/impact_bins_equi_binner.py
@@ -8,19 +8,14 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from utilities import shortest_paths
 from utilities import utils, constants
 from ncflow.lib.problem import Problem
-from alg import geometric_approx_binning
+from alg import approx_water_bet_plus_mcf as equi_binner
 from scripts import benchmark_plot_utils
 
 
 TM_MODEL = 'gravity'
 TOPO_NAME = 'Cogentco.graphml'
 traffic_name_init = "Cogentco.graphml_gravity_1229219359_64.0_76765.140625"
-base_split = .9
 split_type = constants.EXPONENTIAL_DECAY
-num_iter_approx = 1
-num_iter_bet = 500
-U = 0.1
-min_epsilon = 1e-6
 bins_list = [1, 2, 4, 8, 16]
 
 
@@ -37,6 +32,8 @@ assert found
 problem = Problem.from_file(file_name[3], file_name[4])
 link_cap = 1000.0
 
+num_bins, min_epsilon, min_beta, k, link_cap_scale_factor, num_iter_approx, num_iter_bet, base_split = \
+    constants.TOPOLOGY_TO_APPROX_BET_MCF_PARAMS[TOPO_NAME]
 path_output = shortest_paths.all_pair_shortest_path_multi_processor(problem.G, problem.edge_idx,
                                                                     k=num_paths_per_flow, number_processors=32,
                                                                     base_split=base_split,
@@ -57,22 +54,30 @@ danna_fid_to_rate = utils.read_pickle_file(danna_fid_to_rate_mapping[num_paths_p
 for fid, (src, dst, _) in problem.sparse_commodity_list:
     danna_to_fid_rate_vector[fid] = np.sum(danna_fid_to_rate[src, dst])
 
-
 for num_bins in bins_list:
-    max_demand = np.max(problem.traffic_matrix.tm)
-    U = max(U, link_cap / len(problem.sparse_commodity_list))
-    alpha = np.power(max_demand / U, 1.0 / num_bins)
-    print(f"====== alpha {alpha} max demand {max_demand} num bins {num_bins}")
-    gb_output = geometric_approx_binning.max_min_approx(alpha, U, problem, path_dict, link_cap, min_epsilon)
-    gb_fid_to_flow_rate_mapping, duration, run_time_dict = gb_output
+    print(f"num bins {num_bins}")
+    eb_output = equi_binner.get_rates(problem, path_dict, path_edge_idx_mapping,
+                                       path_path_len_mapping, path_total_path_len_mapping,
+                                       path_split_ratio_mapping, num_paths_per_flow,
+                                       num_iter_approx_water=num_iter_approx,
+                                       num_iter_bet=num_iter_bet,
+                                       link_cap=link_cap,
+                                       mcf_grb_method=2,
+                                       num_bins=num_bins,
+                                       min_beta=min_beta,
+                                       min_epsilon=min_epsilon,
+                                       k=k,
+                                       alpha=0,
+                                       link_cap_scale_factor=link_cap_scale_factor)
+    eb_fid_to_flow_rate_mapping, duration, run_time_dict = eb_output
     fairness_no = benchmark_plot_utils.compute_fairness_no_vectorized_baseline(danna_to_fid_rate_vector,
-                                                                               gb_fid_to_flow_rate_mapping,
+                                                                               eb_fid_to_flow_rate_mapping,
                                                                                problem.sparse_commodity_list,
                                                                                theta_fairness=0.1)
 
     total_rate = 0
     for fid, (src, dst, demand) in problem.sparse_commodity_list:
-        total_rate += np.sum(gb_fid_to_flow_rate_mapping[src, dst])
+        total_rate += np.sum(eb_fid_to_flow_rate_mapping[src, dst])
     print(fairness_no, total_rate, duration, run_time_dict)
 
 
